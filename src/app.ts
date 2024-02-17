0cf64387eb706615a9b2b9c06ed2281f47c2a6bb
Samuel Brewer <sam.brewer13@yahoo.com>
2024-02-17 10:56:52 -0700

    keyof constraint

--- src/app.ts
+++ src/app.ts
@@ -17,3 +17,28 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
 
 const mergedObj = merge({ name: 'Samuel' }, { age: 32 });
 mergedObj.age;
+
+interface Lengthy {
+    length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+    let descriptionText = 'Got no value.';
+    if (element.length === 1) {
+        descriptionText = 'Got 1 element';
+    } else if (element.length > 1) {
+        descriptionText = `Got ${element.length} elements`;
+    }
+    return [element, descriptionText];
+}
+
+console.log(countAndDescribe(['Sports', 'Cooking']));
+
+function extractAndConvert<T extends object, U extends keyof T>(
+    obj: T,
+    key: U
+) {
+    return `Value: ${obj[key]}`;
+}
+
+extractAndConvert({ name: 'Samuel' }, 'name');
