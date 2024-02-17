cb9a9b7ae626d63d2e4524edbb195e8eb0906385
Samuel Brewer <sam.brewer13@yahoo.com>
2024-02-17 10:46:03 -0700

    working with constraints

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,19 @@
-const names: Array<string> = ['Samuel', 'Samantha'];
-// names[0].split(' ');
+// const names: Array<string> = ['Samuel', 'Samantha'];
+// // names[0].split(' ');
 
-const promise: Promise<string> = new Promise((resolve) => {
-    setTimeout(() => {
-        resolve('This is done!');
-    }, 2000);
-});
+// const promise: Promise<string> = new Promise((resolve) => {
+//     setTimeout(() => {
+//         resolve('This is done!');
+//     }, 2000);
+// });
 
-promise.then((data) => {
-    data.split(' ');
-});
+// promise.then((data) => {
+//     data.split(' ');
+// });
+
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+    return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({ name: 'Samuel' }, { age: 32 });
+mergedObj.age;
