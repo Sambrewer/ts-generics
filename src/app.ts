e516faeafac91b8425e156f1e46c5ab1a2da63e6
Samuel Brewer <sam.brewer13@yahoo.com>
2024-02-17 11:08:55 -0700

    Generic classes

--- src/app.ts
+++ src/app.ts
@@ -42,3 +42,38 @@ function extractAndConvert<T extends object, U extends keyof T>(
 }
 
 extractAndConvert({ name: 'Samuel' }, 'name');
+
+class DataStorage<T> {
+    private data: T[] = [];
+
+    addItem(item: T) {
+        this.data.push(item);
+    }
+
+    removeItem(item: T) {
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+
+    getItems() {
+        return [...this.data];
+    }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem('Samuel');
+textStorage.addItem('Samantha');
+textStorage.addItem('Bob');
+console.log(textStorage.getItems());
+textStorage.removeItem('Bob');
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+
+const objectStorage = new DataStorage<object>();
+const bobObject = { name: 'Bob' };
+objectStorage.addItem({ name: 'Samuel' });
+objectStorage.addItem({ name: 'Samantha' });
+objectStorage.addItem(bobObject);
+
+objectStorage.removeItem(bobObject);
+console.log(objectStorage.getItems());
