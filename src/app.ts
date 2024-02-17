69ed1787cf1e827f4aaa1fb8179adc88fb39e915
Samuel Brewer <sam.brewer13@yahoo.com>
2024-02-17 11:18:27 -0700

    utility types

--- src/app.ts
+++ src/app.ts
@@ -73,7 +73,29 @@ const objectStorage = new DataStorage<object>();
 const bobObject = { name: 'Bob' };
 objectStorage.addItem({ name: 'Samuel' });
 objectStorage.addItem({ name: 'Samantha' });
+objectStorage.addItem({ name: 'Baby Brewer' });
 objectStorage.addItem(bobObject);
 
 objectStorage.removeItem(bobObject);
 console.log(objectStorage.getItems());
+
+interface CourseGoal {
+    title: string;
+    description: string;
+    completeUntil: Date;
+}
+
+function createCourseGoal(
+    title: string,
+    description: string,
+    date: Date
+): CourseGoal {
+    let courseGoal: Partial<CourseGoal> = {};
+    courseGoal.title = title;
+    courseGoal.description = description;
+    courseGoal.completeUntil = date;
+    return courseGoal as CourseGoal;
+}
+
+const names: Readonly<string[]> = ['Samuel', 'Samantha', 'Baby Brewer'];
+// names.push('Bob');
